0dd76fa46bc9e39d5ece8777af7e579e7c308ff3
Deepankar Malhan <deepankarmalhan@gmail.com>
2017-05-11 18:54:14 -0400

    Added a very basic backend server that will serve up the frontend page

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log(`Server starting!!`);
+var express = require('express');
+var path = require('path');
+var expressApp = express();
+
+const PORT = process.env.PORT || 9000;
+
+expressApp.use(express.static(path.resolve(__dirname, '../client/build')));
+
+expressApp.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+expressApp.listen(PORT, function() {
+  console.log('Listening on port: ', PORT);
+});
