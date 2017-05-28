8808769352d83d15095cafb22eaf850d707f450e
Deepankar Malhan <deepankarmalhan@gmail.com>
2017-05-28 07:32:42 +0530

    Modified the Nav Bar to make it look more clean.

--- client/src/Views/NavBar.js
+++ client/src/Views/NavBar.js
@@ -18,13 +18,25 @@ export default class NavBar extends Component {
       <div className="container">
         <nav className="nav has-shadow">
           <div className="nav-left">
-            <Link className={(this.state.pageType === 'home') ? "nav-item is-active is-tab": "nav-item is-tab"} to="/">{`<Home />`}</Link>
-            <Link className={(this.state.pageType === 'contact') ? "nav-item is-active is-tab": "nav-item is-tab"} to="/contactme">
-              {`<ContactMe />`}
-            </Link>
+            <nav className="level is-mobile">
+              <div className="level-item has-text-centered">
+                <Link className="tabs is-large is-centered" to="/"> {`<DeepankarMalhan/>`}</Link>
+              </div>
+            </nav>
           </div>
 
-          <div className="nav-center">
+          <span className={ (this.state.isMobile) ? 'nav-toggle is-active' : 'nav-toggle' } onClick={ this.toggleMobileNav }>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+
+          <div className={(this.state.isMobile) ? 'nav-right nav-menu is-active' : 'nav-right nav-menu'}>
+            <Link className={(this.state.pageType === 'contact') ? "nav-item is-active is-tab": "nav-item is-tab"} to="/contactme">
+              Contact Me
+            </Link>
+            <Link to="/hobbies" className={(this.state.pageType === 'hobbies') ? "nav-item is-active is-tab" : "nav-item is-tab"}>Hobbies</Link>
+            <Link to="/business" className={(this.state.pageType === 'business') ? "nav-item is-active is-tab" : "nav-item is-tab"}>Business</Link>
             <div className="nav-item">
               <a className="button is-light is-outlined" href="https://github.com/deepankarmalhan/personal-website" target="_blank">
                 <span className="icon">
@@ -35,17 +47,6 @@ export default class NavBar extends Component {
                 </span>
               </a>
             </div>
-          </div>
-
-          <span className={ (this.state.isMobile) ? 'nav-toggle is-active' : 'nav-toggle' } onClick={ this.toggleMobileNav }>
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-
-          <div className={(this.state.isMobile) ? 'nav-right nav-menu is-active' : 'nav-right nav-menu'}>
-            <Link to="/hobbies" className={(this.state.pageType === 'hobbies') ? "nav-item is-active is-tab" : "nav-item is-tab"}>{`<HobbiesCorner/>`}</Link>
-            <Link to="/business" className={(this.state.pageType === 'business') ? "nav-item is-active is-tab" : "nav-item is-tab"}>{`<BusinessStreet/>`}</Link>
           </div>
         </nav>
       </div>
