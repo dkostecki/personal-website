d46e5aa5668440cd707ff3412eb5b532e2e348f1
Deepankar Malhan <deepankarmalhan@gmail.com>
2017-08-02 22:01:06 -0400

    Added a Modal to explain the joke on 404

--- client/src/Views/NotFound.js
+++ client/src/Views/NotFound.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
 import Logo from '../Assets/Images/Logo.png';
 
 export default class NotFound extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  closeModal = () => {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  openModal = () => {
+    this.setState({
+      showModal: true
+    });
+  }
+
   render() {
     return (
       <div className="hero is-fullheight">
         <div className="hero-body">
           <div className="container has-text-centered">
             <img src={Logo} alt="logo"  style={{height:'50%', width:'50%'}}/>
+
+            <ReactModal
+              isOpen={this.state.showModal}
+              contentLabel="What's '/dev/null'?"
+            >
+              <div className="container">
+                <h2 className="subtitle">What is "/dev/null"?</h2>
+                <hr />
+                <div className="content">
+                  <p>
+                    Suppose you run a program on the terminal which is very verbose, and you are not interested in reading through
+                    the entirety of the output. That's where <code>/dev/null</code> comes in; it is a special file on *NIX systems
+                    which takes in a stream of data and converts it to null. This means that the output of a program could be redirected
+                    to that special file and it will not take up additional space on the system, <strong>and not show the output on the terminal!</strong>
+                  </p>
+                </div>
+              </div>
+              <br/>
+              <a className="is-pulled-right" onClick={this.closeModal}><span>Close</span></a>
+            </ReactModal>
 
             <br/>
             <br/>
@@ -17,7 +57,7 @@ export default class NotFound extends Component {
               [ERROR 404: Page doesn't exist]
             </h1>
             <h2 className="subtitle">
-              $ echo "Please stop trying to break the website" > /dev/null
+              $ echo "Please stop trying to break the website" > <a onClick={this.openModal}>/dev/null</a>
             </h2>
             <div className="field has-addons has-addons-centered">
               <p className="control">
