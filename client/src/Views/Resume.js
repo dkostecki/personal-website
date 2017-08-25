4810ef9918b501b16e96a6345a9eabe0ba8c4985
Deepankar Malhan <deepankarmalhan@gmail.com>
2017-08-25 10:31:22 -0400

    Started the Resume page, added a link to my Resume (pdf)

--- client/src/Views/Resume.js
+++ client/src/Views/Resume.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 import Breadcrumb from './Breadcrumb';
+import ResumePDF from '../Assets/PDF/Resume.pdf';
 
 export default class Resume extends Component {
   render () {
     return (
-      <div>
+      <div className="container">
         <Breadcrumb currentPath={this.props.location.pathname} />
-        <h1>Resume rendered here</h1>
+        <h3 className="title">RESUME</h3>
+        <hr/>
+
+        <section className="section">
+            <div className="content">
+              This page is my Resume explained in-depth with all the things I couldn't put on a single page of paper. Before we start, download my portable (not in-depth) Resume
+              version <a href={ResumePDF}>here</a>!
+            </div>
+        </section>
+
       </div>
     );
   }
